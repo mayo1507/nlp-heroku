b176d6b7f357dc9538545a56e5b167df02fc3b8e
mayo1507 <103895782+mayo1507@users.noreply.github.com>
2022-04-20 01:24:01 +0200

    Update nlp-zavrsni.py

--- nlp-zavrsni.py
+++ nlp-zavrsni.py
@@ -200,7 +200,7 @@ def plot_sentiment_barchart(text, method='TextBlob'):
 
 #Entity recognition
 def plot_most_common_named_entity_barchart(text, entity="PERSON"):
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en")
 
     def _get_ner(text, ent):
         doc = nlp(text)
